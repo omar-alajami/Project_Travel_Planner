f897fddc84f8313c0b724299bd112486fb4b7e1a
Omar Al-Ajami <omar.r.ajami@gmail.com>
2026-04-05 02:37:46 +0300

    initial 'main' setup

--- lib/main.dart
+++ lib/main.dart
@@ -10,19 +10,104 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: 'Planner_App',
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: Text('Travel Planner'),
+          centerTitle: true,
         ),
+
+        body: Returned(),
       ),
     );
   }
 }
 
 class Returned extends StatefulWidget{
-  
+  const Returned({super.key});
   @override
-  State<Returned> createState() => 
+  State<Returned> createState() => _ReturnedState();
 }
 
+class _ReturnedState extends State<Returned>{
+
+  String dest = '';
+  int nbOfPeople = 0;
+  int duration = 0;
+  int budget = 0;
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 100),
+            TextField(
+              decoration: InputDecoration(labelText: 'Destination'),
+              onSubmitted: (gotDest){dest = gotDest;},
+            ),
+
+            SizedBox(height: 50),
+            TextField(
+              keyboardType: TextInputType.numberWithOptions(),
+              decoration: InputDecoration(labelText: 'Nb of People'),
+              onSubmitted: (gotNb){nbOfPeople = int.tryParse(gotNb) ?? 0;},//make a function that handles if input is different, then call that function passing gotNb as parameter, instead of writing the whole function here.
+            ),
+
+            SizedBox(height: 50),
+            TextField(
+              keyboardType: TextInputType.numberWithOptions(),
+              decoration: InputDecoration(labelText: 'Duration'),
+              onSubmitted: (gotDur){duration = int.tryParse(gotDur) ?? 0;},
+            ),
+
+            SizedBox(height: 50),
+            TextField(
+              decoration: InputDecoration(labelText: 'Enter buget (min for minimum or keep empty to disregard)'),
+              onSubmitted: (gotBud){budget = parseBud(gotBud);},
+            ),
+
+            SizedBox(height: 50),
+            TextButton(onPressed: (){getOutput();}, child: Text('SUBMIT'))
+          ]
+        )
+      )
+    );
+  }
+
+  void getOutput(){
+    String shownString = '';
+    try{
+      Trip trip = Trip(dest, nbOfPeople, duration, budget);
+      setState((){
+        shownString = trip.availability();
+      });
+    } catch(E){
+      setState((){
+        shownString = 'ERROR\n$E';
+      });
+    }
+
+    showDialog(
+      context: context,
+      builder: (context){
+        return AlertDialog(
+          title: Text('OUTPUT'),
+          content: Text(shownString),
+        );
+      }
+    );
+  }
+  
+
+  int parseBud(String a){
+    late int b;
+    if (a == 'min'){b = 0;}
+    else if (a == '' || a == 'backdoor') {b = -1;}
+    else {b = int.tryParse(a) ?? -2;}
+
+    return b;
+  }
+}
